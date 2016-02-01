184ad8df86d952b9765d4e446f09762db6bd6a59
Tom Carpenter <strange_orange_fish@hotmail.com>
2016-02-01 18:20:06 -0500

    Correct code indenting
    Correct the code indenting so that it uses the standard 4 spaces per tab
    rather than the Arduino IDEs nonsense choice of 2 spaces per tab. This
    makes editing the code in things like Notepad++ or UECIDE much easier.

--- AstroEQ-Firmware/EEPROMReader.cpp
+++ AstroEQ-Firmware/EEPROMReader.cpp
@@ -2,54 +2,48 @@
 #include <avr/eeprom.h>
 #include "EEPROMReader.h"
  
-byte EEPROM_readByte(byte address)
-{
-  return eeprom_read_byte((byte*) address);
+byte EEPROM_readByte(byte address) {
+    return eeprom_read_byte((byte*) address);
 }
 
-unsigned int EEPROM_readInt(byte address)
-{
-  TwoBytes fetcher;
-  fetcher.array[0] = EEPROM_readByte(address);
-  fetcher.array[1] = EEPROM_readByte(address+1);
-  return fetcher.integer;
+unsigned int EEPROM_readInt(byte address) {
+    TwoBytes fetcher;
+    fetcher.array[0] = EEPROM_readByte(address);
+    fetcher.array[1] = EEPROM_readByte(address+1);
+    return fetcher.integer;
 }
-unsigned long EEPROM_readLong(byte address)
-{
-  FourBytes fetcher;
-  fetcher.array[0] = EEPROM_readInt(address);
-  fetcher.array[1] = EEPROM_readInt(address+2);
-  return fetcher.integer;
+unsigned long EEPROM_readLong(byte address) {
+    FourBytes fetcher;
+    fetcher.array[0] = EEPROM_readInt(address);
+    fetcher.array[1] = EEPROM_readInt(address+2);
+    return fetcher.integer;
 }
 
-void EEPROM_readString(char* string, byte len, byte address)
-{
-  for(byte i = 0; i < len; i++) {
-    string[i] = EEPROM_readByte(address++);
-  }
+void EEPROM_readString(char* string, byte len, byte address) {
+    for(byte i = 0; i < len; i++) {
+        string[i] = EEPROM_readByte(address++);
+    }
 }
 
-void EEPROM_writeByte(byte val, byte address)
-{
-  return eeprom_write_byte((byte*) address, val);
+void EEPROM_writeByte(byte val, byte address) {
+    return eeprom_write_byte((byte*) address, val);
 }
 
-void EEPROM_writeInt(unsigned int val, byte address)
-{
-  TwoBytes storer = {val};
-  EEPROM_writeByte(storer.array[0], address);
-  EEPROM_writeByte(storer.array[1], address+1);
+void EEPROM_writeInt(unsigned int val, byte address) {
+    TwoBytes storer = {val};
+    EEPROM_writeByte(storer.array[0], address);
+    EEPROM_writeByte(storer.array[1], address+1);
 }
-void EEPROM_writeLong(unsigned long val, byte address)
-{
-  FourBytes storer = {val};
-  EEPROM_writeInt(storer.array[0], address);
-  EEPROM_writeInt(storer.array[1], address+2);
+
+void EEPROM_writeLong(unsigned long val, byte address) {
+    FourBytes storer = {val};
+    EEPROM_writeInt(storer.array[0], address);
+    EEPROM_writeInt(storer.array[1], address+2);
 }
-void EEPROM_writeString(const char* string, byte len, byte address)
-{
-  for(byte i = 0; i < len; i++) {
-    EEPROM_writeByte(string[i], address+i);
-  }
+
+void EEPROM_writeString(const char* string, byte len, byte address) {
+    for(byte i = 0; i < len; i++) {
+        EEPROM_writeByte(string[i], address+i);
+    }
 }
 
--- AstroEQ-Firmware/synta.h
+++ AstroEQ-Firmware/synta.h
@@ -1,27 +1,28 @@
 
 #ifndef synta_h
 #define synta_h
-  #include "AstroEQ.h"
-  #include "commands.h"
-  
-  void synta_initialise(unsigned long version);
-  void synta_assembleResponse(char* dataPacket, char commandOrError, unsigned long responseData);
-  char synta_recieveCommand(char* dataPacket, char character);
-  byte synta_axis(byte axis = 2); //make target readonly to outside world.
-  char synta_command(); //make current command readonly to outside world.
-  unsigned long synta_hexToLong(char* hex);
-  byte synta_hexToByte(char* hex);
-  
-  //Methods for accessing command variables - now in commands.h
-  //void cmd_setDir(byte target, byte _dir);
-  //void cmd_updateStepDir(byte target, byte stepSize);
-  //unsigned int cmd_fVal(byte target);
-  //void cmd_setStopped(byte target, byte _stopped);
-  //void cmd_setGotoEn(byte target, byte _gotoEn);
-  //void cmd_setFVal(byte target, byte _FVal);
-  //void cmd_setjVal(byte target, unsigned long _jVal);
-  //void cmd_setIVal(byte target, unsigned int _IVal);
-  //void cmd_setHVal(byte target, unsigned long _HVal);
-  //void cmd_setGVal(byte target, byte _GVal);
+
+#include "AstroEQ.h"
+#include "commands.h"
+
+void synta_initialise(unsigned long version, byte gVal);
+void synta_assembleResponse(char* dataPacket, char commandOrError, unsigned long responseData);
+char synta_recieveCommand(char* dataPacket, char character);
+byte synta_axis(byte axis = 2); //make target readonly to outside world.
+char synta_command(); //make current command readonly to outside world.
+unsigned long synta_hexToLong(char* hex);
+byte synta_hexToByte(char* hex);
+
+//Methods for accessing command variables - now in commands.h
+//void cmd_setDir(byte target, byte _dir);
+//void cmd_updateStepDir(byte target, byte stepSize);
+//unsigned int cmd_fVal(byte target);
+//void cmd_setStopped(byte target, byte _stopped);
+//void cmd_setGotoEn(byte target, byte _gotoEn);
+//void cmd_setFVal(byte target, byte _FVal);
+//void cmd_setjVal(byte target, unsigned long _jVal);
+//void cmd_setIVal(byte target, unsigned int _IVal);
+//void cmd_setHVal(byte target, unsigned long _HVal);
+//void cmd_setGVal(byte target, byte _GVal);
   
 #endif
