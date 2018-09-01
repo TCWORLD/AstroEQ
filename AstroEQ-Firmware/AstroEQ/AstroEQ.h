a4073553527bda86f9c3cf24c154b201846d707d
Tom Carpenter <strange_orange_fish@hotmail.com>
2018-09-01 23:02:12 +0100

    Possible fix to Indilib-GVal Handling
    In some rare cases, :f commands are sent from indilib after updating the :GVal, but before initialising a movement.
    
    AstroEQ doesn't normally update its high-speed gear flag until a movement starts, which can confuse indilib into thinking high-speed wasn't supported (I think?).
    
    Anyway, this fix allows the :G command to update the high speed flag immediately if the motors aren't running.

--- AstroEQ-Firmware/AstroEQ/AstroEQ.h
+++ AstroEQ-Firmware/AstroEQ/AstroEQ.h
@@ -126,6 +126,10 @@ typedef uint8_t byte;
 #define ST4N (1)  //Negative ST4 Pin
 #define ST4O (-1) //Neither ST4 Pin
 
+#define MOTION_START_NOTREADY   0
+#define MOTION_START_REQUESTED  1
+#define MOTION_START_UPDATABLE  2
+
 #define MIN_IVAL 50
 #define MAX_IVAL 1200
 
