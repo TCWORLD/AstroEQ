230e0e00e0e096e067c22619672e0f3b40aeb612
Jeff Slight <jslight90@gmail.com>
2018-08-31 20:41:27 -0700

    Fix typos

--- AstroEQ-Firmware/AstroEQ/AstroEQ.h
+++ AstroEQ-Firmware/AstroEQ/AstroEQ.h
@@ -133,9 +133,9 @@ typedef uint8_t byte;
 
 #define nop() __asm__ __volatile__ ("nop \n\t")
 
-#define GVAL_HIGHSPEED = 0
-#define GVAL_LOWSPEED_SLEW = 1
-#define GVLA_LOWSPEED_GOTO = 2
+#define GVAL_HIGHSPEED 0
+#define GVAL_LOWSPEED_SLEW 1
+#define GVAL_LOWSPEED_GOTO 2
 
 /*
  * Standalone Pin Names
