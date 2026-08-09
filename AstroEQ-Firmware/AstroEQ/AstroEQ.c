34d9e6ccbf04508e055f6473193cb0873bc34670
Tom Carpenter <T.Carpenter@leeds.ac.uk>
2026-08-09 16:03:25 +0100

    Fix command :M returning error
    While the command is unused, we must return a success status other EQMOD will immediately abort any go-to movement.

--- AstroEQ-Firmware/AstroEQ/AstroEQ.c
+++ AstroEQ-Firmware/AstroEQ/AstroEQ.c
@@ -1286,6 +1286,7 @@ bool decodeCommand(char command, char* buffer){ //each command is axis specific.
                 readyToGo[axis] = MOTION_START_NOTREADY;
             }
             break;
+        case 'M': //Go-To midpoint is unused. But ensure it is a recognized command.
         case 'J': //Start operation. Will be handled after response sent, but ensure it is a recognized command.
             if (progMode != RUNMODE) { //only allow movements to be initiated outside of programming mode.
                 command = '\0'; //force sending of error packet!.
